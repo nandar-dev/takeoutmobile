2c81059c5c80c1c7d23331401af50ccd2913a671
Nanda <mgnandar290@gmail.com>
2025-04-09 20:48:22 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:takeout/pages/auth/otp_page.dart';
-import 'package:takeout/pages/profile/profile_page.dart';
+import 'package:takeout/pages/landing/landing_page.dart';
 import 'package:takeout/theme/app_colors.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      // home: LandingPage(),
-      home: ProfilePage(),
+      home: LandingPage(),
+      // home: ProfilePage(),
       theme: ThemeData(
         appBarTheme: AppBarTheme(backgroundColor: AppColors.appbarBackground),
         scaffoldBackgroundColor: AppColors.background,
